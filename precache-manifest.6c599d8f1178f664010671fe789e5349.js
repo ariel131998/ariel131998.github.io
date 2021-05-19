3839ac484614e99663d764965736dcc12ae3baec
Ariel <ariel.ortega.zitle98@gmail.com>
2021-05-18 21:36:18 -0500

    Updates

---
+++ precache-manifest.6c599d8f1178f664010671fe789e5349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87760f13464177995601b07919ba495",
+    "url": "/GitHub-Education-Portfolio/index.html"
+  },
+  {
+    "revision": "7b68dbe4f0cc198a0cbb",
+    "url": "/GitHub-Education-Portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36dc4c21ac5707915aff",
+    "url": "/GitHub-Education-Portfolio/static/js/2.1288cb2e.chunk.js"
+  },
+  {
+    "revision": "7b68dbe4f0cc198a0cbb",
+    "url": "/GitHub-Education-Portfolio/static/js/main.cb180012.chunk.js"
+  },
+  {
+    "revision": "5e7d53efc42e27e2deba",
+    "url": "/GitHub-Education-Portfolio/static/js/runtime~main.60ee6b4b.js"
+  },
+  {
+    "revision": "2d20937546043ee3c34238471fab3177",
+    "url": "/GitHub-Education-Portfolio/static/media/profile.2d209375.jpg"
+  }
+]);
